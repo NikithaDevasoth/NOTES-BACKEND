7a09d9929669be95cd2b180c3cc539f51ce3329d
NikithaDevasoth <devasothnikitha09@gmail.com>
2025-09-04 20:07:30 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "notes-frontend-lkgp.vercel.app",
     })
   );
 }
@@ -33,10 +33,10 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
@@ -44,4 +44,5 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
   });
+
 });
